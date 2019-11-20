07445f4cd364f4ebf1a818ab33d3e5ad11940af3
reikoch <8436725+reikoch@users.noreply.github.com>
2019-11-20 15:53:13 +0100

    report contents of directory of source bundles

---
+++ R/write_repo.R
@@ -0,0 +1,16 @@
+# updates to the model repo
+
+#' list packages provided in srccontrib directory
+#'
+#' @param srccontrib directory of source R package bundles
+#'
+#' @return dataframe sorted by package name
+#' @export
+#'
+#' @examples
+report_src_contrib <- function(srccontrib='src/contrib') {
+  bundles <- list.files(path = srccontrib, pattern = '*.tar.gz')
+  structure(data.frame(t(sapply(strsplit(sub('.tar.gz$', '', bundles), split='_'), rbind)), stringsAsFactors = FALSE),
+                  names=c('Package', 'Version'))
+}
+
