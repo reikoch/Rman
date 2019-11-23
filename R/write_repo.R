52d555664a153e92ffd9f62b69d3bf4b7f9ffce5
reikoch <8436725+reikoch@users.noreply.github.com>
2019-11-23 16:43:40 +0100

    allow reporting of source bundle directory

--- R/write_repo.R
+++ R/write_repo.R
@@ -5,15 +5,28 @@
 #' @param srccontrib directory of source R package bundles
 #'
 #' @return dataframe sorted by package name
-#' @export
 #'
 #' @examples
-report_src_contrib <- function(srccontrib='src/contrib') {
+list_src_contrib <- function(srccontrib='src/contrib') {
   bundles <- list.files(path = srccontrib, pattern = '*.tar.gz')
   structure(data.frame(t(sapply(strsplit(sub('.tar.gz$', '', bundles), split='_'), rbind)), stringsAsFactors = FALSE),
                   names=c('Package', 'Version'))
 }
 
-# yy <- merge(xx, baserecpkgs, all=TRUE)
-# yy$Version[is.na(yy$Version)] <- as.character(getRversion())
-
+#' report packages provided in srccontrib directory plus current base packages
+#'
+#' @param srccontrib directory of source R package bundles
+#' @param report name of tsv file result is written to, 
+#'        default to file R-x.y.z_pkgs.tsv  
+#'
+#' @return dataframe sorted by package name
+#' @export
+#'
+#' @examples
+report_src_contrib <- function(srccontrib='src/contrib', report=paste0('R-', getRversion(),'_pkgs.tsv')) {
+  yy <- merge(list_src_contrib(srccontrib), baserecpkgs, all=TRUE, by='Package')
+  yy$Version[is.na(yy$Version)] <- as.character(getRversion())
+  yy <- yy[order(yy$Package),]
+  write.table(yy, file=report, row.names = FALSE, sep = '\t', na = '', quote = FALSE)
+  yy
+}
